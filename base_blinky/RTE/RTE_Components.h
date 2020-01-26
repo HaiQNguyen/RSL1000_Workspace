079e2be7fe8425aa31d675940e40cd33e79f4f0d
Hai Nguyen <qnguyen@arroweurope.com>
2020-01-26 23:34:40 +0100

    Init commit
    Add git ignore, add base project and add document

---
+++ base_blinky/RTE/RTE_Components.h
@@ -0,0 +1,19 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: base_blinky
+ * RTE configuration: base_blinky.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "rsl10.h"
+
+/* ONSemiconductor::Device.Libraries.GPIO.source */
+#define RTECFG_GPIO
+
+#endif /* RTE_COMPONENTS_H */
